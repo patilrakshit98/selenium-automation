f72999b3203abb558f06b191100483f89aec5957
patilrakshit98 <patilrakshit98@gmail.com>
2026-08-13 01:40:20 +0530

    Update driver for headless

--- src/main/java/driver/DriverFactory.java
+++ src/main/java/driver/DriverFactory.java
@@ -2,10 +2,16 @@ package driver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 	public static WebDriver createDriver() {
-		return new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless=new");
+		options.addArguments("--no-sandbox");
+		options.addArguments("disable-dev-shm-usage");
+		options.addArguments("--window-size=1920,1080");
+		return new ChromeDriver(options);
 	}
 
 }
