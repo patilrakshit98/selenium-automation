0faddac03aba9d7ecf1d604e8c99e67e0480687e
patilrakshit98 <patilrakshit98@gmail.com>
2026-08-13 12:12:26 +0530

    Trying to fix headless mode

--- src/main/java/driver/DriverFactory.java
+++ src/main/java/driver/DriverFactory.java
@@ -10,10 +10,13 @@ public class DriverFactory {
 
         ChromeOptions options = new ChromeOptions();
 
-        //options.addArguments("--headless=new");
+        options.addArguments("--disable-blink-features=AutomationControlled");
+        options.addArguments("--start-maximized");
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-popup-blocking");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--window-size=1920,1080");
 
         return new ChromeDriver(options);
     }
